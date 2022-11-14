612c6104aedce30417796ddba606bfeb747a5d1d
BSal_Windows <ma.salazar@globe.com.ph>
2022-11-14 11:07:22 +0800

    added notes lang

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -50,32 +50,43 @@ authRouter.post(
         .withMessage("sign in reqs a valid email"),
 ], 
 async (request, response) => {
+    // uses express validators to verify request's response
+    // if no error sa response, tuloy lang if meron show it
     const errors = validationResult(request);
     if (!errors.isEmpty()) {
       response.status(400).json({ errors: errors.array() });
       return;
     }
     
+    // pick lets u pick out fields you want lang from the request body to use in ur code
+    // or to "hawak" muna
     const filteredBody = pick(request.body, ["email", "password"]);
+
+    //checks if user nanag sign in is existing or nah
     const user = await request.app.locals.prisma.user.findUnique({
         where: { email: filteredBody.email}
     })
 
+    //if user does not exisit
     if (!user) {
         response.status(404).json({ data:null, message: "error user not found"})
         return
     }
     // compare pw si bcrypt na dun
+    // for is user exists, need to check if pw nya is tama by comparing the pw they put
+    // in vs sa naka save sa db na hashed kanina
+    // checks if pw ni filteredbody (yung pinang sign up) == pw ni user (yung pinang sign in)
     const isComparePassword = await bcrypt.compare(
         filteredBody.password,
         user.password
     )
-
+    
+    // say this if di same pw
     if (!isComparePassword) {
         response.status(401).json({ data:null, message: "wrong pw"})
     }
     // response.send({ data:user, message: "ok"})
-    // para result mo wala yung password
+    // para result mo wala yung password use omit to omit showing the id and pw ng user
     const filteredUser = omit(user, ["id", "password"])
     response.send({ data:filteredUser, message: "ok"})
 })
